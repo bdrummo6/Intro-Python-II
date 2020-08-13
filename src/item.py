d8720b433b47c882822b094266d35a08a1ebcb80
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-13 19:10:06 -0400

    Update item.py
    Added a __str__ method to the Items class.

--- src/item.py
+++ src/item.py
@@ -4,3 +4,6 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        return f'\t- name: {self.name}\n\t\t description: {self.description}'
