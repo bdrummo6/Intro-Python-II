b5e255bb2d219ef80b505b7d4c0e6065c35a12c6
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-13 19:09:35 -0400

    Update player.py
    Added the items attribute to the Player class.

--- src/player.py
+++ src/player.py
@@ -1,25 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items=None):
+        if items is None:
+            items = []
         self.name = name
         self.current_room = current_room
+        self.items = items
 
     def __str__(self):
         return f'{self.name}, {self.current_room}'
-
-    def _set_name_(self, name):
-        self._name = name
-
-    def _get_name_(self):
-        return self._name
-
-    def _set_room_(self, current_room):
-        self._current_room = current_room
-
-    def _get_room_(self):
-        return self._current_room
-
-    name = property(_get_name_, _set_name_)
-    current_room = property(_get_room_, _set_room_)
