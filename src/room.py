a3a5e64c36125db85346982602c252f5b4d9b3c4
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-13 21:29:19 -0400

    Update room.py

--- src/room.py
+++ src/room.py
@@ -1,6 +1,5 @@
 # Implement a class to hold room information. This should have name and description attributes.
 
-from lightsource import LightSource
 
 class Room:
     def __init__(self, name, description, is_light, items=None):
@@ -16,7 +15,7 @@ class Room:
         self.n_to = None
 
     def __str__(self):
-        return f'{self.name}\n\t- description: {self.description}'
+        return f'{self.name}.\n\t- description: {self.description}'
 
     def add_item(self, item):
         self.items.append(item)
